897f4f04e611d711f1ab67344e68d31ca9626410
Bruce Leong <brucewleong@gmail.com>
2021-08-12 07:24:45 -0700

    8.12.21 - 724am - latest update

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const fs = require("fs");
+// function rqListener(req, res) {
+
+// }
+
+http.createServer(function(req, res) { //event driven function
+  console.log(req)
+});
