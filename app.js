f1971e35b3a3ddbd8b838fa7ec4f7353250f607a
Bruce Leong <brucewleong@gmail.com>
2021-08-14 07:43:08 -0700

    res/req write

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ const fs = require("fs");
 
 // }
 
-http.createServer(function(req, res) { //event driven function
-  console.log(req)
+const server = http.createServer((request, response) => {
+  /*
+  Event loop - on going listener - event driven server. Purpose, single threaded JS execution
+  */
+  const {
+    url,
+    method,
+    headers
+  } = request;
+  if (url === "/") {
+    response.write("<html>");
+    response.write("<head><title>Test Page</title></head>");
+    response.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>test</button></input></form></body>")
+    response.write("</html>");
+    return response.end();
+  }
+  response.setHeader("Content-type", "text/html");
+  response.write("<html>");
+  response.write("<head><title>Test Page</title></head>");
+  response.write("<body><h1>new page</h1></body>")
+  response.write("</html>");
+  response.end();
+  // process.exit(); //close/kill request
 });
+server.listen(3000); //creating server
